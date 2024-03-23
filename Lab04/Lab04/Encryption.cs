125876fe3579491c155fd8efac7d011eafcd8e07
Anastasia <nastya.golodok@mail.ru>
2024-03-23 13:20:54 +0300

    fourth lab

---
+++ Lab04/Lab04/Encryption.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    public class Encryption
+    {
+        public static string ReadFromFile(string fileName)
+        {
+            string path = "D:\\Univer\\6sem\\IS\\Lab04\\";
+            string filePath = Path.Combine(path, fileName);
+
+            var reader = new StreamReader(filePath);
+            string text = reader.ReadToEnd().ToLower();
+
+            return text;
+        }
+
+        public static string WriteToFile(string fileName, string text)
+        {
+            string path = "D:\\Univer\\6sem\\IS\\Lab04\\";
+            string filePath = Path.Combine(path, fileName);
+            File.WriteAllText(filePath, text);
+            return filePath;
+        }
+
+        //////////////
+        public static string EncryptCaesar(string text, string keyword)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            string result = "";
+            int keywordIndex = 0;
+
+            foreach (char symbol in text)
+            {
+                if (char.IsLetter(symbol))
+                {
+                    char offset = char.IsUpper(symbol) ? 'A' : 'a';
+                    int shift = keyword[keywordIndex % keyword.Length] - offset;
+
+                    char encryptedSymbol = (char)((symbol + shift - offset + 26) % 26 + offset);
+                    result += encryptedSymbol;
+
+                    keywordIndex++;
+                }
+                else
+                {
+                    result += symbol;
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"\nВремя выполнения операции: {stopWatch.Elapsed}");
+
+
+            return result;
+        }
+
+        public static string DecryptCaesar(string text, string keyword)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            string result = "";
+            int keywordIndex = 0;
+
+            foreach (char symbol in text)
+            {
+                if (char.IsLetter(symbol))
+                {
+                    char offset = char.IsUpper(symbol) ? 'A' : 'a';
+                    int shift = keyword[keywordIndex % keyword.Length] - offset;
+
+                    char decryptedSymbol = (char)((symbol - shift - offset + 26) % 26 + offset);
+                    result += decryptedSymbol;
+
+                    keywordIndex++;
+                }
+                else
+                {
+                    result += symbol;
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"\nВремя выполнения операции: {stopWatch.Elapsed}");
+
+
+            return result;
+        }
+        /// //////////////////////////////////////////////////////////////
+
+        public static char[,] GenerateTable(string keyword)
+        {
+            const int rows = 4;
+            const int columns = 8;
+            char[,] table = new char[rows, columns];
+            var index = 0;
+
+            keyword = keyword.ToLower();
+
+            foreach (var c in keyword.Distinct())
+            {
+                table[index / columns, index % columns] = c;
+                index++;
+            }
+
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                if (index >= rows * columns)
+                    break;
+                if (!keyword.Contains(c))
+                {
+                    table[index / columns, index % columns] = c;
+                    index++;
+                }
+            }
+
+            return table;
+        }
+
+
+        public static string EncryptTrithemius(string text)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            const int rows = 4;
+            const int columns = 8;
+            char[,] table = GenerateTable("Anastasiya"); 
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            foreach (char symbol in text.ToLower()) 
+            {
+                if (char.IsLetter(symbol))
+                {
+                    int row = -1, column = -1;
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < columns; j++)
+                        {
+                            if (table[i, j] == symbol)
+                            {
+                                row = i;
+                                column = j;
+                                break;
+                            }
+                        }
+                        if (row != -1) 
+                            break;
+                    }
+
+                    if (row != -1 && column != -1)
+                    {
+                        char encryptedSymbol = table[(row + 1) % rows, column];
+                        encryptedText.Append(encryptedSymbol);
+                    }
+                    else
+                    {
+                        encryptedText.Append(symbol);
+                    }
+                }
+                else
+                {
+                    encryptedText.Append(symbol);
+                }
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"\nВремя выполнения операции: {stopWatch.Elapsed}");
+
+            return encryptedText.ToString();
+        }
+
+
+
+        public static string DecryptTrithemius(string fileName)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            const int rows = 4;
+            const int columns = 8;
+            char[,] table = GenerateTable("Anastasiya"); 
+
+            StringBuilder decryptedText = new StringBuilder();
+
+            foreach (char symbol in fileName.ToLower()) 
+            {
+                if (char.IsLetter(symbol))
+                {
+                    int row = -1, column = -1;
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < columns; j++)
+                        {
+                            if (table[i, j] == symbol)
+                            {
+                                row = i;
+                                column = j;
+                                break;
+                            }
+                        }
+                        if (row != -1) 
+                            break;
+                    }
+
+                    if (row != -1 && column != -1)
+                    {
+                        char decryptedSymbol = table[(row - 1 + rows) % rows, column];
+                        decryptedText.Append(decryptedSymbol);
+                    }
+                    else
+                    {
+                        decryptedText.Append(symbol);
+                    }
+                }
+                else
+                {
+                    decryptedText.Append(symbol);
+                }
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"\nВремя выполнения операции: {stopWatch.Elapsed}");
+
+            return decryptedText.ToString();
+        }
+
+
+
+        public static Dictionary<char, int> CountFrequancies(string fileName)
+        {
+            var frequancies = new Dictionary<char, int>();
+
+            foreach (var item in fileName)
+            {
+                if (frequancies.ContainsKey(item))
+                {
+                    frequancies[item] += 1;
+                }
+                else
+                {
+                    frequancies.Add(item, 1);
+                }
+            }
+            return frequancies;
+        }
+
+        public static string GetLetters(string text)
+        {
+            var nonLet = new HashSet<char>() { ',', '.', '/', '?', '!', '=', '+', '-', '\'', '\"', '(', ')', ':', ';', '\n', '\0', '/', '\t', ' ', '«', '»', '—', '\r', '’', '☻', '–' };
+
+            StringBuilder newText = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                if (!nonLet.Contains(c))
+                {
+                    newText.Append(c);
+                }
+            }
+
+            return newText.ToString();
+        }
+
+        public static void ShowDictionary(Dictionary<char, int> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value} | ");
+            }
+        }
+
+
+        public static void ShowDictionary(Dictionary<char, double> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value} | ");
+            }
+        }
+
+        public static Dictionary<char, double> GetDictionarySymb(Dictionary<char, int> dict, string sourceText)
+        {
+            var dictionaryProb = new Dictionary<char, double>();
+
+            foreach (var item in dict)
+            {
+                dictionaryProb.Add(item.Key, (double)item.Value / (double)sourceText.Length);
+            }
+            return dictionaryProb;
+        }
+    }
+}
---
+++ Lab04/Lab04/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using Lab04;
+
+namespace Lab04
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keywordSurname = "Golodok";
+            string textPath = "lab4.txt";
+            string textPath2 = "lab4_3.txt";
+            string textWrite = "lab4_2.txt";
+            string textWrite2 = "lab4_4.txt";
+
+            string text = Encryption.ReadFromFile(textPath);
+            string encrText = Encryption.ReadFromFile(textWrite);
+
+            string text2 = Encryption.ReadFromFile(textPath2);
+            string encrText2 = Encryption.ReadFromFile(textWrite2);
+
+
+
+            string encrLetters = Encryption.GetLetters(encrText);
+            string decrLetters = Encryption.GetLetters(text);
+            Dictionary<char, int> encrFreq = Encryption.CountFrequancies(encrLetters);
+            Dictionary<char, int> decrFreq = Encryption.CountFrequancies(decrLetters);
+
+
+            Console.WriteLine("Исходный текст: \n" + text);
+            Console.WriteLine("____________________________________________________________________");
+                      
+            string encrCaesarText = Encryption.EncryptCaesar(text, keywordSurname);
+            Console.WriteLine("\nЗашифрованный текст: \n" + encrCaesarText);
+            Encryption.WriteToFile(textWrite, encrCaesarText);
+            Console.WriteLine("\nЧастота появления символов");
+
+            var dictEncr = Encryption.GetDictionarySymb(encrFreq, encrLetters);
+            Encryption.ShowDictionary(dictEncr);
+
+            Console.WriteLine("____________________________________________________________________");
+
+
+            string decrCaesarText = Encryption.DecryptCaesar(encrCaesarText, keywordSurname);
+            Console.WriteLine("\nРасшифрованный текст: \n" + decrCaesarText);
+
+            Console.WriteLine("\nЧастота появления символов");
+
+            var dictDecr = Encryption.GetDictionarySymb(decrFreq, decrLetters);
+            Encryption.ShowDictionary(dictDecr);
+
+            Console.WriteLine("____________________________________________________________________");
+
+            string encrTrisText = Encryption.EncryptTrithemius(text2);
+            Console.WriteLine("\nЗашифрованный текст: \n" + encrTrisText);
+            Encryption.WriteToFile(textWrite2, encrTrisText);
+
+            Console.WriteLine("____________________________________________________________________");
+
+            string decrTrisText = new string(Encryption.DecryptTrithemius(encrTrisText));
+            Console.WriteLine("\nРасшифрованный текст: \n" + text);
+
+        }
+    }
+}
