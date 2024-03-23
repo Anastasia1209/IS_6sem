08146480254d2d805d5779064935f33ec4033407
Anastasia <nastya.golodok@mail.ru>
2024-03-23 13:20:23 +0300

    third lab

---
+++ Lab03/Lab03/Calculations.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    public class Calculations
+    {
+        public static int CalculateGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int CalculateGCD(int a, int b, int c)
+        {
+            return CalculateGCD(a, CalculateGCD(b, c));
+        }
+
+        public static bool IsPrime(int num)
+        {
+            if (num <= 1) return false;
+            if (num <= 3) return true;
+            if (num % 2 == 0 || num % 3 == 0) return false;
+            for (int i = 5; i * i <= num; i += 6)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static (List<int>, int) FindPrimes(int n, int m)
+        {
+            List<int> primes = new List<int>();
+            int count = 0;
+            for (int i = m; i <= n; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primes.Add(i);
+                    count++;
+                }
+            }
+            return (primes, count);
+        }
+
+        public static List<int> PrimeFactors(int num)
+        {
+            List<int> factors = new List<int>();
+            int divisor = 2;
+            while (num > 1)
+            {
+                while (num % divisor == 0)
+                {
+                    factors.Add(divisor);
+                    num /= divisor;
+                }
+                divisor++;
+            }
+            return factors;
+        }
+
+        public static string CanonForm(int num)
+        {
+            List<int> factors = PrimeFactors(num);
+            return $"{num} = {string.Join(" * ", factors)}";
+        }
+
+        public static bool IsConcatenatedNumberPrime(int m, int n)
+        {
+            int mLength = m.ToString().Length;
+            int nLength = n.ToString().Length;
+
+            int concatenatedNumber = m * (int)Math.Pow(10, nLength) + n;
+
+            return IsPrime(concatenatedNumber);
+        }
+
+    }
+}
---
+++ Lab03/Lab03/Program.cs
@@ -0,0 +1,62 @@
+﻿using Lab03;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 49;
+        int num2 = 70;
+        int gcdTwo = Calculations.CalculateGCD(num1, num2);
+        Console.WriteLine($"НОД чисел {num1} и {num2} равен {gcdTwo}");
+
+        int num3 = 84;
+        int gcdThree = Calculations.CalculateGCD(num1, num2, num3);
+        Console.WriteLine($"НОД чисел {num1}, {num2} и {num3} равен {gcdThree}");
+        //////////////
+
+        Console.Write("Введите верхнюю границу для поиска простых чисел: ");
+        int a = int.Parse(Console.ReadLine());
+        int b = 2;
+        var (primes, count) = Calculations.FindPrimes(a, b);
+        Console.WriteLine($"Количество простых чисел: {count}");
+        Console.WriteLine($"Простые числа до {a}: {string.Join(", ", primes)}");
+        ///////////////
+
+        double expCount = a / Math.Log(a);
+        Console.WriteLine($"Ожидаемое количество простых чисел: {expCount}");
+        /////////////
+
+        Console.WriteLine("------------------------------------");
+
+        int n = 401, m = 367;
+        var (primesn, countn) = Calculations.FindPrimes(n, m);
+        Console.WriteLine($"Количество простых чисел от 367 до 401: {countn}");
+        Console.WriteLine($"Простые числа до {n}: {string.Join(", ", primesn)}");
+        ///////////////
+
+        double expCountn = n / Math.Log(n) - m / Math.Log(m);
+        Console.WriteLine($"Ожидаемое количество простых чисел: {expCountn}");
+
+        string numCanon = Calculations.CanonForm(num1);
+        string mCanon = Calculations.CanonForm(m);
+        string nCanon = Calculations.CanonForm(n);
+        Console.WriteLine($"Каноническая форма записи числа m: {numCanon}");
+        Console.WriteLine($"Каноническая форма записи числа m: {mCanon}");
+        Console.WriteLine($"Каноническая форма записи числа n: {nCanon}");
+        /////////////
+        
+        if (Calculations.IsConcatenatedNumberPrime(m, n))
+        {
+            Console.WriteLine($"Число, состоящее из конкатенации цифр {m} и {n}, является простым.");
+        }
+        else
+        {
+            Console.WriteLine($"Число, состоящее из конкатенации цифр {m} и {n}, не является простым.");
+        }
+        ////////////
+        int gcd = Calculations.CalculateGCD(n, m);
+        Console.WriteLine($"НОД чисел {n} и {m} равен {gcd}");
+
+    }
+}
