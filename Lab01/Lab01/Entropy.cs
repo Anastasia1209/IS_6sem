508a220bcf4daecf4b9883ee7b7781857a80e143
Anastasia <nastya.golodok@mail.ru>
2024-03-23 13:19:09 +0300

    first lab

---
+++ Lab01/Lab01/Entropy.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public class Entropy
+    {
+        public static Dictionary<char, int> CountFrequancies(string fileName)
+        {
+            var frequancies = new Dictionary<char, int>();
+
+            foreach (var item in fileName)
+            {
+                if (frequancies.ContainsKey(item))
+                {
+                    frequancies[item] += 1;
+                }
+                else
+                {
+                    frequancies.Add(item, 1);
+                }
+            }
+            return frequancies;
+        }
+
+        public static double CalculateEntropy(string fileName)
+        {
+            Dictionary<char, int> frequencies = CountFrequancies(fileName);
+            int totalCharecters = frequencies.Sum(item => item.Value);
+            double entropy = 0;
+
+            foreach(var item in frequencies)
+            {
+                double probability = (double)item.Value / totalCharecters; 
+                entropy -= probability * Math.Log(probability, 2);
+            }
+            return entropy;
+        }
+
+        public static double GetInformation(double entropy, string fileName)
+        {
+            return entropy * fileName.Length;
+        }
+         
+        public static double CalculateEffectiveEntropy(double p, double entropy, bool isBinary)
+        {
+            var q = 1 - p;
+            double result;
+
+            if (isBinary && (p == 1 || p == 0))
+                return 1;
+            if (isBinary)
+                entropy = 1;
+            if (!isBinary && p == 1)
+                return 0;
+
+            result = (-p * Math.Log2(p)) - q * Math.Log2(q);
+
+            return entropy - result;
+        }
+
+        public static string ReadFromFile(string fileName)
+        {
+            string path = "D:\\Univer\\6sem\\IS\\Lab01\\Lab01";
+            string filePath = Path.Combine(path, fileName);
+
+            var reader = new StreamReader(filePath);
+            string text = reader.ReadToEnd().ToLower();
+
+            return text;
+        }
+
+        public static string GetLetters(string text)
+        {
+            var nonLet = new HashSet<char>() { ',', '.', '/', '?', '!', '=', '+', '-', '\'', '\"', '(', ')', ':', ';', '\n', '\0', '/', '\t', ' ', '«', '»', '—', '\r', '’', '☻', '–' };
+
+            StringBuilder newText = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                if (!nonLet.Contains(c))
+                {
+                    newText.Append(c);
+                }
+            }
+
+            return newText.ToString();
+        }
+
+        public static void ShowDictionary(Dictionary<char, int> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value} | ");
+            }
+        }
+
+        public static void ShowDictionary(Dictionary<char, double> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value} | ");
+            }
+        }
+
+        public static Dictionary<char, double> GetDictionarySymb(Dictionary<char, int> dict, string sourceText)
+        {
+            var dictionaryProb = new Dictionary<char, double>();
+
+            foreach (var item in dict)
+            {
+                dictionaryProb.Add(item.Key, (double)item.Value / (double)sourceText.Length);
+            }
+            return dictionaryProb;
+        }
+    }
+}
---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,66 @@
+﻿using Lab01;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string rusText = Entropy.ReadFromFile("rus.txt");
+        string rusLetters = Entropy.GetLetters(rusText);
+        Dictionary<char, int> rusLettersFrequency = Entropy.CountFrequancies(rusLetters);
+        Entropy.ShowDictionary(rusLettersFrequency);
+
+        double rusEntropy = Entropy.CalculateEntropy(rusLetters);
+        Console.WriteLine($"Энтропия русского алфавита(кирилица): {rusEntropy}");
+
+        string engText = Entropy.ReadFromFile("eng.txt");
+        string engLetters = Entropy.GetLetters(engText);
+        double engEntropy = Entropy.CalculateEntropy(engLetters);
+        Console.WriteLine($"Энтропия английского алфавита(латиница): {engEntropy}\n");
+
+        string rusBinText = Entropy.ReadFromFile("rusBin.txt");
+        string rusBinLetters = Entropy.GetLetters(rusBinText);
+        double rusBinEntropy = Entropy.CalculateEntropy(rusBinLetters);
+        Console.WriteLine($"Энтропия русского алфавита в бинарном представлении: {rusBinEntropy}");
+
+        string engBinText = Entropy.ReadFromFile("engBin.txt");
+        string engBinLetters = Entropy.GetLetters(engBinText);
+        double engBinEntropy = Entropy.CalculateEntropy( engBinLetters);
+        Console.WriteLine($"Энтропия английского алфавита в бинарном представлении: {engBinEntropy}\n");
+
+        string rusFIO = Entropy.ReadFromFile("fio.txt");
+        double infoNum = Entropy.GetInformation(rusEntropy, rusFIO);
+        Console.WriteLine($"Количество информации в ФИО на русском: {infoNum}");
+
+        string rusBinFIO = Entropy.ReadFromFile("fioBin.txt");
+        double infoBinNum = Entropy.GetInformation(rusBinEntropy, rusBinFIO);
+        Console.WriteLine($"Количество информации в ФИО в бинарном формате: {infoBinNum}\n");
+
+
+        double effectiveEntropyWithProb = Entropy.CalculateEffectiveEntropy(0.1, rusBinEntropy, true);
+        Console.WriteLine($"Количество информации при 0.1 вероятности ошибки в бинарном формате : {Entropy.GetInformation(effectiveEntropyWithProb, rusBinFIO)}");
+
+        effectiveEntropyWithProb = Entropy.CalculateEffectiveEntropy(0.1,rusEntropy, false);
+        Console.WriteLine($"Количество информации при 0.1 вероятности ошибки в формате русского алфавита: {Entropy.GetInformation(effectiveEntropyWithProb, rusFIO)}\n");
+
+        effectiveEntropyWithProb = Entropy.CalculateEffectiveEntropy(0.5, rusBinEntropy, true);
+        Console.WriteLine($"Количество информации при 0.5 вероятности ошибки в двоичном формате : {Entropy.GetInformation(effectiveEntropyWithProb, rusBinFIO)}");
+
+        effectiveEntropyWithProb = Entropy.CalculateEffectiveEntropy(0.5, rusEntropy, false);
+        Console.WriteLine($"Количество информации при 0.5 вероятности ошибки в формате русского алфавита: {Entropy.GetInformation(effectiveEntropyWithProb, rusFIO)}\n");
+
+        effectiveEntropyWithProb = Entropy.CalculateEffectiveEntropy(1.0, rusBinEntropy, true);
+        Console.WriteLine($"Количество информации при 1.0 вероятности ошибки в двоичном формате : {Entropy.GetInformation(effectiveEntropyWithProb, rusBinFIO)}");
+
+        effectiveEntropyWithProb = Entropy.CalculateEffectiveEntropy(1.0, rusEntropy, false);
+        Console.WriteLine($"Количество информации при 1.0 вероятности ошибки в формате русского алфавита: {Entropy.GetInformation(effectiveEntropyWithProb, rusFIO)}\n");
+
+        Dictionary<char, int> engLettersFrequency = Entropy.CountFrequancies(engLetters);
+
+
+        var dict = Entropy.GetDictionarySymb(engLettersFrequency, engLetters);
+        Entropy.ShowDictionary(dict);
+
+
+    }
+}
