edc5324e1365583fb78053201175f047f93e2e69
Anastasia <nastya.golodok@mail.ru>
2024-05-16 11:47:05 +0300

    lab9

---
+++ Lab09/Lab09/Operations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    public class Operations
+    {
+        public static int NOD(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            return a;
+        }
+
+        public static int Inverse(int a, int m)
+        {
+            a = a % m;
+            for (int x = 1; x < m; x++)
+                if ((a * x) % m == 1)
+                    return x;
+            return 1;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
