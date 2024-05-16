0423dab035cd9b80b8bea1a01e9464b9f4996db1
Anastasia <nastya.golodok@mail.ru>
2024-05-16 11:49:03 +0300

    lab10

---
+++ lab10/lab10_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using lab10_2;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace lab10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger p = BigInteger.Parse("6788923193742832419212371231");
+            BigInteger q = BigInteger.Parse("3902847768726378127361823571");
+
+            string Message = "Golodok Anastasiya";
+
+            RSA.algorithmRSA(p, q, Message);
+            Console.WriteLine();
+            ElGamal.algorithmElGamal(5935711, Message);
+        }
+    }
+}
